dd0e5639123396f5a59119f11b6bdb98cc53417f
MohammedBhatti <39573979+MohammedBhatti@users.noreply.github.com>
2018-07-18 14:03:10 -0400

    Create challenge1.py

---
+++ challenge1.py
@@ -0,0 +1,8 @@
+string = "this is a string of some words"
+string_list = string.split(' ')
+d = {}
+# result = [transform iteration filter]
+print[s for s in string_list]
+
+d = {s:len(s) for s in string_list}
+print(d)
